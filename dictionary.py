74b740e3def3b4a18cf462582bf8848f76837e52
Martha EK. Demanya <m.demanya@bsl.com.gh>
2023-06-14 13:15:22 +0000

    solution to dictionary challenge

--- dictionary.py
+++ dictionary.py
@@ -3,9 +3,26 @@
 mydict = {"Mon": "Monday",
           "Tues": "Tuesday",
           "Wed": "Wednesday",
-          "Thurs": "Thursday"}
+          "Thurs": "Thursday",
+          "Fri": "Friday",
+          "Sat": "Saturday",
+          "Sun": "Sunday"}
 
-print(mydict)
+# print(mydict)
 
-mydict["Sun"] = "Sunday"
-print(mydict)
+# mydict["Sun"] = "Sunday"
+# print(mydict)
+
+# del mydict["Mon"]  # removing an item from a dictionary
+# print(mydict)
+
+while True:
+    dict_key = input("Please enter a day: ")
+    if dict_key == "quit":
+        break
+    if dict_key in mydict:
+        description = mydict.get(dict_key)
+        print(description)
+    else:
+        print(f"We don't have a {dict_key}")
+
