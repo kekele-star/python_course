8241e563f4a775b1b800b7975ac6b39a6c0bb2e0
Martha EK. Demanya <m.demanya@bsl.com.gh>
2023-06-15 16:04:44 +0000

    turtle module

---
+++ turtle_module.py
@@ -0,0 +1,14 @@
+# The turtle module in Python is a graphics library that allows you to create 
+# and control turtle graphics. Turtle graphics are a popular way to introduce 
+# programming to beginners, as it provides a simple and intuitive way to draw
+# shapes and patterns on the screen using a turtle cursor.
+from turtle import *
+
+done = "Well done, you have finished your drawing"
+
+forward(150)
+right(250)
+forward(150)
+
+done()
+print(done)
