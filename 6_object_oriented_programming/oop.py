70e2c85fe10fe1130d36ef2fba31063debf21259
just_kekele <esinamdemanya@gmail.com>
2023-09-08 14:53:37 +0000

    add oop code

---
+++ 6_object_oriented_programming/oop.py
@@ -0,0 +1,10 @@
+# Object Oriented Programming in Python
+
+
+
+class Programmer(object):
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
